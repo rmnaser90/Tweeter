d15aca86fc96a157dd7737cc905ba4f2225de125
rmnaser90 <rmnaser90@gmail.com>
2020-11-09 10:53:54 +0200

    Format code according to liors instructions

--- controller.js
+++ controller.js
@@ -4,42 +4,26 @@ const input = $('#input')
 const postIt = function () {
     tweeter.addPost(input.val())
     input.val('')
-    // console.log(tweeter.getPosts());
+
     render()
 }
 
-
-
 $('#postsContainer').on('click', ".commentIt", function () {
-    let postID = $(this).closest(".post").data('id')
-    let text = $(this).closest('.post').find('.inputComment').val()
+    const postID = $(this).closest(".post").data('id')
+    const text = $(this).closest('.post').find('.inputComment').val()
     tweeter.addComment(text, postID)
     render()
-
-
 })
 
-
 $('#postsContainer').on('click', ".delete-comment", function () {
-    let postID = $(this).closest(".post").data('id')
-    let commentID = $(this).closest(".comment").data('id')
+    const postID = $(this).closest(".post").data('id')
+    const commentID = $(this).closest(".comment").data('id')
     tweeter.removeComment(postID, commentID)
-
     render()
 })
 
 $('#postsContainer').on('click', ".deletePost", function () {
-    let postID = $(this).closest(".post").data('id')
-
+    const postID = $(this).closest(".post").data('id')
     tweeter.removePost(postID)
-
     render()
 })
-
-
-
-
-
-
-
-
--- logic.js
+++ logic.js
@@ -21,33 +21,26 @@ const Tweeter = function () {
         }
     ]
 
-
     let postIdCounter = posts.length
     let commentIdCounter = 0;
     for (const post of posts) {
         commentIdCounter += post.comments.length
     }
-    // console.log(postIdCounter);
-    // console.log(commentIdCounter);
 
     const getPosts = function () {
         return posts
     }
     const addPost = function (text) {
         postIdCounter++
-        let newPost = { text, id: 'p' + postIdCounter, comments: [] }
+        const newPost = { text, id: 'p' + postIdCounter, comments: [] }
         posts.push(newPost)
-
     }
     const removePost = function (id) {
-
         for (const i in posts) {
             if (posts[i].id == id) {
                 posts.splice(i, 1)
-
             }
         }
-
     }
 
     const addComment = function (text, postId) {
@@ -59,8 +52,6 @@ const Tweeter = function () {
 
             }
         }
-
-
     }
 
     const removeComment = function (postId, commentId) {
@@ -75,8 +66,6 @@ const Tweeter = function () {
 
             }
         }
-
-
     }
 
     return {
@@ -90,62 +79,3 @@ const Tweeter = function () {
 }
 
 const tweeter = Tweeter()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const tweeter = Tweeter()
-// tweeter.addPost("hello")
-// console.log(tweeter.getPosts());
-
-// tweeter.removePost('p1')
-// tweeter.addPost("hi")
-// tweeter.addComment('p4','good to see you')
-// tweeter.removeComment('p2','c6')
-// console.log(tweeter.getPosts());
-
-// console.log(tweeter.getPosts())
-
-// tweeter.addPost("This is my own post!")
-// console.log(tweeter.getPosts())
-// // This should be added to the posts array:
-// // {text: "This is my own post!", id: "p3", comments: []}
-
-// tweeter.removePost("p1")
-// console.log(tweeter.getPosts())
-// //There should only be two posts in the post's array:
-// //{text: "Aw man, I wanted to be first", id: "p2", comments: Array(3)}
-// //{text: "This is my own post!", id: "p3", comments: []}
-
-// // //============================
-// // //============================
-// // //Stop here
-// // //Make sure everything works. Then keep going
-// // //============================
-// // //============================
-
-// tweeter.addComment("Damn straight it is!", "p3")
-// tweeter.addComment("Second the best!", "p2")
-// console.log(tweeter.getPosts())
-// //This should be added to the third post's comments array:
-// //{id: "c7", text: "Damn straight it is!"}
-
-// // //This should be added to the second post's comments array:
-// // //{id: "c8", text: "Second the best!"}
-
-// tweeter.removeComment("p2", "c6")
-// console.log(tweeter.getPosts())
-// //This comment should be removed:
-// //{id: "c6", text: "Haha second place what a joke."}
